3a0da760497821b65c8717f5a3ac7ba21fdf8ccd
Dawid Tyburek <davidtyburek@gmail.com>
2021-06-15 16:12:35 +0200

    Add fragments and navigation

--- app/src/main/kotlin/com/kerubyte/retropunk/ui/fragment/favourites/FavouritesFragment.kt
+++ app/src/main/kotlin/com/kerubyte/retropunk/ui/fragment/favourites/FavouritesFragment.kt
@@ -1,4 +1,19 @@
 package com.kerubyte.retropunk.ui.fragment.favourites
 
-class FavouritesFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kerubyte.retropunk.R
+
+class FavouritesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favourites, container, false)
+    }
 }
--- app/src/main/kotlin/com/kerubyte/retropunk/ui/fragment/search/SearchFragment.kt
+++ app/src/main/kotlin/com/kerubyte/retropunk/ui/fragment/search/SearchFragment.kt
@@ -1,4 +1,19 @@
 package com.kerubyte.retropunk.ui.fragment.search
 
-class SearchFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kerubyte.retropunk.R
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
 }
