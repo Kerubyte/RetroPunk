ee1d15bdaac69aec3c5ef49772d2ceed95a8ab07
Dawid Tyburek <davidtyburek@gmail.com>
2021-06-23 07:31:23 +0200

    Add adapter and viewHolder layout

---
+++ app/src/main/kotlin/com/kerubyte/retropunk/ui/adapter/MainAdapter.kt
@@ -0,0 +1,54 @@
+package com.kerubyte.retropunk.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.kerubyte.retropunk.R
+import com.kerubyte.retropunk.domain.model.Beer
+
+class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            LayoutInflater.from(
+                parent.context
+            ).inflate(
+                R.layout.list_row,
+                parent,
+                false
+            )
+        )
+    }
+    
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val product = differ.currentList[position]
+        holder.itemView.apply {
+            holder.title.text = product.name
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private val differCallback = object : DiffUtil.ItemCallback<Beer>() {
+        override fun areItemsTheSame(oldItem: Beer, newItem: Beer): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Beer, newItem: Beer): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.text_product_name)
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+}
